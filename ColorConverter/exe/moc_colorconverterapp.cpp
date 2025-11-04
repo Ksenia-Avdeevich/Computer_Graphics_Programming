e9a6991813360b03373acc38618343caa8107345
Ksenia-Avdeevich <avdeevich.ksenia@gmail.com>
2025-11-04 10:33:44 +0300

    добавление exe файла

---
+++ ColorConverter/exe/moc_colorconverterapp.cpp
@@ -0,0 +1,138 @@
+/****************************************************************************
+** Meta object code from reading C++ file 'colorconverterapp.h'
+**
+** Created by: The Qt Meta Object Compiler version 69 (Qt 6.9.0)
+**
+** WARNING! All changes made in this file will be lost!
+*****************************************************************************/
+
+#include "../../../colorconverterapp.h"
+#include <QtGui/qtextcursor.h>
+#include <QtCore/qmetatype.h>
+
+#include <QtCore/qtmochelpers.h>
+
+#include <memory>
+
+
+#include <QtCore/qxptype_traits.h>
+#if !defined(Q_MOC_OUTPUT_REVISION)
+#error "The header file 'colorconverterapp.h' doesn't include <QObject>."
+#elif Q_MOC_OUTPUT_REVISION != 69
+#error "This file was generated using the moc from 6.9.0. It"
+#error "cannot be used with the include files from this version of Qt."
+#error "(The moc has changed too much.)"
+#endif
+
+#ifndef Q_CONSTINIT
+#define Q_CONSTINIT
+#endif
+
+QT_WARNING_PUSH
+QT_WARNING_DISABLE_DEPRECATED
+QT_WARNING_DISABLE_GCC("-Wuseless-cast")
+namespace {
+struct qt_meta_tag_ZN17ColorConverterAppE_t {};
+} // unnamed namespace
+
+template <> constexpr inline auto ColorConverterApp::qt_create_metaobjectdata<qt_meta_tag_ZN17ColorConverterAppE_t>()
+{
+    namespace QMC = QtMocConstants;
+    QtMocHelpers::StringRefStorage qt_stringData {
+        "ColorConverterApp",
+        "updateFromRGB",
+        "",
+        "updateFromCMYK",
+        "updateFromHSV",
+        "openColorPicker",
+        "updateColorDisplay",
+        "onRGBTextChanged",
+        "onCMYKTextChanged",
+        "onHSVTextChanged"
+    };
+
+    QtMocHelpers::UintData qt_methods {
+        // Slot 'updateFromRGB'
+        QtMocHelpers::SlotData<void()>(1, 2, QMC::AccessPrivate, QMetaType::Void),
+        // Slot 'updateFromCMYK'
+        QtMocHelpers::SlotData<void()>(3, 2, QMC::AccessPrivate, QMetaType::Void),
+        // Slot 'updateFromHSV'
+        QtMocHelpers::SlotData<void()>(4, 2, QMC::AccessPrivate, QMetaType::Void),
+        // Slot 'openColorPicker'
+        QtMocHelpers::SlotData<void()>(5, 2, QMC::AccessPrivate, QMetaType::Void),
+        // Slot 'updateColorDisplay'
+        QtMocHelpers::SlotData<void()>(6, 2, QMC::AccessPrivate, QMetaType::Void),
+        // Slot 'onRGBTextChanged'
+        QtMocHelpers::SlotData<void()>(7, 2, QMC::AccessPrivate, QMetaType::Void),
+        // Slot 'onCMYKTextChanged'
+        QtMocHelpers::SlotData<void()>(8, 2, QMC::AccessPrivate, QMetaType::Void),
+        // Slot 'onHSVTextChanged'
+        QtMocHelpers::SlotData<void()>(9, 2, QMC::AccessPrivate, QMetaType::Void),
+    };
+    QtMocHelpers::UintData qt_properties {
+    };
+    QtMocHelpers::UintData qt_enums {
+    };
+    return QtMocHelpers::metaObjectData<ColorConverterApp, qt_meta_tag_ZN17ColorConverterAppE_t>(QMC::MetaObjectFlag{}, qt_stringData,
+            qt_methods, qt_properties, qt_enums);
+}
+Q_CONSTINIT const QMetaObject ColorConverterApp::staticMetaObject = { {
+    QMetaObject::SuperData::link<QMainWindow::staticMetaObject>(),
+    qt_staticMetaObjectStaticContent<qt_meta_tag_ZN17ColorConverterAppE_t>.stringdata,
+    qt_staticMetaObjectStaticContent<qt_meta_tag_ZN17ColorConverterAppE_t>.data,
+    qt_static_metacall,
+    nullptr,
+    qt_staticMetaObjectRelocatingContent<qt_meta_tag_ZN17ColorConverterAppE_t>.metaTypes,
+    nullptr
+} };
+
+void ColorConverterApp::qt_static_metacall(QObject *_o, QMetaObject::Call _c, int _id, void **_a)
+{
+    auto *_t = static_cast<ColorConverterApp *>(_o);
+    if (_c == QMetaObject::InvokeMetaMethod) {
+        switch (_id) {
+        case 0: _t->updateFromRGB(); break;
+        case 1: _t->updateFromCMYK(); break;
+        case 2: _t->updateFromHSV(); break;
+        case 3: _t->openColorPicker(); break;
+        case 4: _t->updateColorDisplay(); break;
+        case 5: _t->onRGBTextChanged(); break;
+        case 6: _t->onCMYKTextChanged(); break;
+        case 7: _t->onHSVTextChanged(); break;
+        default: ;
+        }
+    }
+    (void)_a;
+}
+
+const QMetaObject *ColorConverterApp::metaObject() const
+{
+    return QObject::d_ptr->metaObject ? QObject::d_ptr->dynamicMetaObject() : &staticMetaObject;
+}
+
+void *ColorConverterApp::qt_metacast(const char *_clname)
+{
+    if (!_clname) return nullptr;
+    if (!strcmp(_clname, qt_staticMetaObjectStaticContent<qt_meta_tag_ZN17ColorConverterAppE_t>.strings))
+        return static_cast<void*>(this);
+    return QMainWindow::qt_metacast(_clname);
+}
+
+int ColorConverterApp::qt_metacall(QMetaObject::Call _c, int _id, void **_a)
+{
+    _id = QMainWindow::qt_metacall(_c, _id, _a);
+    if (_id < 0)
+        return _id;
+    if (_c == QMetaObject::InvokeMetaMethod) {
+        if (_id < 8)
+            qt_static_metacall(this, _c, _id, _a);
+        _id -= 8;
+    }
+    if (_c == QMetaObject::RegisterMethodArgumentMetaType) {
+        if (_id < 8)
+            *reinterpret_cast<QMetaType *>(_a[0]) = QMetaType();
+        _id -= 8;
+    }
+    return _id;
+}
+QT_WARNING_POP
